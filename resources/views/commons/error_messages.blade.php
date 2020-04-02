c44f7f5c5b1f1d9704590939a67ba5eda3a3485e
nobutoshi.yamamoto <sokeinobu@icloud.com>
2020-04-02 07:15:44 +0000

    user registration

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -2,6 +2,6 @@
     <ul class="alert alert-danger" role="alert">
         @foreach ($errors->all() as $error)
             <li class="ml-4">{{ $error }}</li>
-        @endforeach
+        @endforeach    
     </ul>
 @endif
